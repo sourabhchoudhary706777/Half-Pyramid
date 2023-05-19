7083e7a28d629ea3d83cf6c56a712b4679a92cb0
Sourabh Choudhary <sourabhchoudhary706777@gmail.com>
2023-05-19 10:53:28 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+  
+public class Hello
+  {
+        public static void main(String a[])
+    {  int n=5;
+     
+      for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=i;j++)
+        {
+          int sum = i+j;
+          if(sum%2==0)
+          {
+            System.out.print(1+" ");
+          }
+          else 
+          {
+            System.out.print(0+" ");
+          }
+      }
+      
+      System.out.println();
+
+  }
+    }}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
